8d153b0eae7063eb0d131ba41b90f0176a468f0c
Felix Schneider <felix.schneider@campus.lmu.de>
2023-09-06 16:36:27 +0200

    Debugging

--- spikeinterface/sorters/kilosortbase.py
+++ spikeinterface/sorters/kilosortbase.py
@@ -171,7 +171,7 @@ class KilosortBase:
                 matlab_command = f'''\nmatlab.orig -nodisplay -nosplash -r "'''
                 if sorter_job_params:
                     if 'memory_limit' in sorter_job_params.keys():
-                        shell_cmd += f'''\nulimit -v {sorter_job_params['memory_limit']}'''
+                        shell_cmd += f'''\nulimit -v {int(sorter_job_params['memory_limit'])}'''
                     if 'gpuDevice' in sorter_job_params.keys():
                         matlab_command += f'''gpuDevice({sorter_job_params['gpuDevice']});'''
 
